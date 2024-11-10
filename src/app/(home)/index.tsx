2b1c5ea3c94d81d58e45441461c9a090538700e5
thangngo <ngosythang123456@gmail.com>
2024-11-10 16:40:55 +0700

    feat: store task

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -1,14 +1,34 @@
 import { Link } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
+import { getAllTasks, storeTasksList, Task } from "../../store/task";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const tasks = await getAllTasks();
+      if (tasks === null) {
+        await storeTasksList();
+      }
+      setTasks(tasks);
+    };
+    fetchData();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Hello Home</Text>
-      <Link asChild href="/task/123">
-        <Button mode="contained">Task 123</Button>
-      </Link>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Link href={`/task/${item.id}`}>Open Task</Link>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
